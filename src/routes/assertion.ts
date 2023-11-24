fcc86893705e1aa5df6984cfd83c3cc331fafa22
kg0r0 <kg0r0@yahoo.co.jp>
2023-11-24 22:53:43 +0900

    Add /assertion/result

--- src/routes/assertion.ts
+++ src/routes/assertion.ts
@@ -1,19 +1,25 @@
 import express, { Request, Response } from 'express';
 import {
-  GenerateAuthenticationOptionsOpts,
   generateAuthenticationOptions,
   verifyAuthenticationResponse,
 } from '@simplewebauthn/server';
+import type {
+  GenerateAuthenticationOptionsOpts,
+  VerifiedAuthenticationResponse,
+  VerifyAuthenticationResponseOpts,
+} from '@simplewebauthn/server';
 import database from './db';
 import config from './config';
+import { AuthenticationResponseJSON } from '@simplewebauthn/server/script/deps';
+import { isoBase64URL, isoUint8Array } from '@simplewebauthn/server/helpers';
 
 const router = express.Router();
 
 const rpID = config.rpID;
+const origin = config.origin;
 
 router.post('/options', async (req: Request, res: Response) => {
   const user = database[req.body.username];
-  console.log(user);
   if (!user) {
     res.json({
       status: 'failed',
@@ -37,11 +43,59 @@ router.post('/options', async (req: Request, res: Response) => {
     errorMessage: '',
   };
   const options = Object.assign(successRes, credentialGetOptions);
+  req.session.username = user.username;
   req.session.currentChallenge = options.challenge;
   res.json(options)
 });
 
 router.post('/result', async (req: Request, res: Response) => {
+  const body: AuthenticationResponseJSON = req.body;
+  const expectedChallenge = req.session.currentChallenge;
+  const username = `${req.session.username}`;
+  const user = database[username];
+
+  let dbAuthenticator;
+  const bodyCredIDBuffer = isoBase64URL.toBuffer(body.rawId)
+  for (const authenticator of user.authenticators) {
+    if (isoUint8Array.areEqual(authenticator.credentialID, bodyCredIDBuffer)) {
+      dbAuthenticator = authenticator;
+      break;
+    }
+  }
+
+  if (!dbAuthenticator) {
+    return res.status(400).send({
+      status: 'failed',
+      errorMessage: 'Authenticator is not registered with this site.',
+    });
+  }
+
+  let verification: VerifiedAuthenticationResponse;
+  try {
+    const opts: VerifyAuthenticationResponseOpts = {
+      response: body,
+      expectedChallenge: `${expectedChallenge}`,
+      expectedOrigin: origin,
+      expectedRPID: rpID,
+      authenticator: dbAuthenticator,
+      requireUserVerification: true
+    };
+    verification = await verifyAuthenticationResponse(opts);
+  } catch (error: any) {
+    return res.status(400).send({
+      status: 'failed',
+      errorMessage: error.message,
+    });
+  }
+  const { verified, authenticationInfo } = verification;
+  if (!verified || !authenticationInfo) {
+    return res.status(400).send({
+      status: 'failed',
+      errorMessage: 'Can not authenticate signature.',
+    });
+  }
+  dbAuthenticator.counter = authenticationInfo.newCounter;
+  req.session.currentChallenge = undefined;
   const result = {
     status: 'ok',
     errorMessage: '',
